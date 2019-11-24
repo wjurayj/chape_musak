6f66c19a464b7a06a4eeba89d63f978181efd4b3
Katie Friis <katiefriis@gmail.com>
2019-11-24 13:32:19 -0500

    outline files

---
+++ main.py
@@ -0,0 +1,29 @@
+import tensorflow as tf
+import numpy as np
+
+
+class Model(tf.keras.Model):
+    def __init__(self, vocab_size):
+        pass
+
+
+    def call(self, inputs, initial_state):
+        pass
+
+    def loss(self, logits, labels):
+        pass
+
+
+
+def train(model, train_inputs, train_labels):
+    pass
+
+def test(model, test_inputs, test_labels):
+    pass
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
---
+++ preprocess.py
@@ -0,0 +1,6 @@
+import tensorflow as tf
+import numpy as np
+
+
+def get_data(train_file, test_file):
+    pass
